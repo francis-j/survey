baf8bf0d8e24a38293dc8130194c13bab0696549
Francis Jones <france100@gmail.com>
2017-11-21 01:33:27 +0000

    New enum for question type. Updated Question model

--- src/app/models/Question.ts
+++ src/app/models/Question.ts
@@ -1,9 +1,10 @@
 import { QuestionResponse } from "./QuestionResponse";
+import { QuestionType } from "../enums/question-type.enum";
 
 export class Question {
     id: number;
     number: number;
     text: string;
     responses: Array<QuestionResponse>;
-    type: number;
+    type: QuestionType;
 }
